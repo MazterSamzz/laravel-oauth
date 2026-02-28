5d0b01e8af2bf498db733404db9b2fe993fada4d
MazterSamzz <8948689+MazterSamzz@users.noreply.github.com>
2026-02-28 13:17:37 +0700

    4. Register & Auto Login - Dasar Membuat Fitur Login Belum jadi

--- apps/laravel-oauth/app/Http/Controllers/AuthController.php
+++ apps/laravel-oauth/app/Http/Controllers/AuthController.php
@@ -4,19 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function index()
+    public function login()
     {
         return view('auth.login');
     }
 
-    public function login(Request $request)
+    public function loginPost(Request $request)
     {
         $credentials = $request->validate([
             'email' => 'required|email|min:5|max:50',
-            'password' => 'required|max:50',
+            'password' => 'required|min:5|max:50',
         ]);
 
         if (Auth::attempt($credentials, $request->remember)) {
@@ -33,5 +35,26 @@ class AuthController extends Controller
     {
         Auth::logout();
         return to_route('login');
+    }
+
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function registerPost(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|email|unique:users,email|min:5|max:50',
+            'password' => 'required|confirmed|min:5|max:50',
+        ]);
+
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+        $data['status'] = 'active';
+        User::create($data);
+
+        return to_route('login')->with('success', 'Registration successful, please login');
     }
 }
